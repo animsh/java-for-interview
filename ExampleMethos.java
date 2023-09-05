34d9ab77eca88cb104bb04763478a47968f32c0c
Sagar More <animsh.more@gmail.com>
2023-09-05 11:40:27 +0530

    11 - Writing & Using Methods in Java

---
+++ ExampleMethos.java
@@ -0,0 +1,10 @@
+public class ExampleMethos {
+
+    public static void greetOthers(String name) {
+        System.out.println("Hii!, I'm " + name);
+    }
+
+    public static void main(String[] args) {
+        greetOthers("Animesh");
+    }
+}
